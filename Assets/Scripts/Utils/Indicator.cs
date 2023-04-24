7decf5c79b90741b647ee8544c1996698ca55643
Alex Gázquez <alexgazquezmonedero@gmail.com>
2023-04-24 19:35:42 +0200

    - Estructura básica de carpetas - Creación de las escenas - Configuración de Cinemachine - Configuración de VFX - Configuración de URP - Importe TMP Essentials - Creación de Materiales - Diseño básico de escena

---
+++ Assets/Scripts/Utils/Indicator.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+[System.Serializable]
+public class Indicator
+{
+    //Valores
+    [SerializeField]
+    private int currentValue;
+    public int maxValue;
+    public int initValue;
+
+    //Gestion recuperación/deterioro automatico
+    public int autoUpdateRate = 0;
+    public int UpdateRateSeconds = 1;
+    public bool autoUpdate = false;
+    //Coroutine
+    public Coroutine AutoUpdateCoroutine;
+    public Image icon;
+    
+    //Inicialización
+    public virtual void RestartStats()
+    {
+        CurrentValue = initValue;
+        autoUpdateRate = 0;
+    }
+
+    //Operaciones
+    public float GetPercentage()
+   {
+       return CurrentValue / maxValue;
+   }
+
+   //Get y Set publico
+   public int CurrentValue
+   {
+       get => currentValue;
+       set
+       {
+           currentValue = value >= 0 ? value : 0;
+           if (OnIndicatorChange != null)
+               OnIndicatorChange(value);
+       }
+   }
+   public void setAutoUpdate(bool newState)
+   {
+        this.autoUpdate = newState;
+   }
+   
+
+    //Eventos para detectar cambios en el indicador
+    public delegate void OnIndicatorChangeDelegate(int newValue);
+    public event OnIndicatorChangeDelegate OnIndicatorChange;
+}
